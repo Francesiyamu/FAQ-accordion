eb168e5f78d31495852036dd9a326360b0d12461
Kelvin-Ben <kevoben14@gmail.com>
2025-04-23 03:51:27 +0300

    Create javascript functionality to toggle faq cards

--- JS/faq.js
+++ JS/faq.js
@@ -1,25 +1,20 @@
+document.querySelectorAll('.faq .btn').forEach((button) => {
+    button.addEventListener('click', function () {
+        const answerId = this.getAttribute('aria-controls');
+        const answer = document.getElementById(answerId);
+        const isExpand = this.getAttribute('aria-expanded') === 'true';
+        console.log(isExpand)
+        console.log(answer.innerText)
+        const plusIcon = this.querySelector('.icon-plus');
+        const minusIcon = this.querySelector('.icon-minus');
 
-//you want to select all the faq elements and add an event listener to each one
-//to toggle the visibliity of the content and change the icon when the button is clicked
-document.querySelectorAll('.faq').forEach(faq => {
-    const button = faq.querySelector('.btn button');
-    const content = faq.querySelector('article');
-    const plusIcon = faq.querySelector('.image');
+        answer.classList.toggle('visible');
 
-    button.addEventListener('click', () => {
-         // Check if the content is currently hidden
-         if (content.hidden) {
-            //Show the content
-            content.hidden = false;
+        
+        this.setAttribute('aria-expanded', !isExpand);
 
-            // Change the icon to the minus icon
-            plusIcon.src = 'assets/images/icon-minus.svg';
-        } else {
-            // Hide the content
-            content.hidden = true; 
-
-            // Change the icon to the plus icon
-            plusIcon.src = 'assets/images/icon-plus.svg';
-        }
-    });
-});
+        // toggle icons
+        minusIcon.classList.toggle('hidden');
+        plusIcon.classList.toggle('hidden');
+    })
+})
